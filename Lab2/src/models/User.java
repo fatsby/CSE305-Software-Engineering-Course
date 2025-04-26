7fbf64f0fff329792f6196cc0c83c9f10a2f941f
thien-0408 <nguyendinhthien933@gmail.com>
2025-04-26 09:05:53 +0700

    update
    zozozozo

--- Lab2/src/models/User.java
+++ Lab2/src/models/User.java
@@ -1,5 +1,20 @@
 package models;
 
 public class User {
-    int userID;
+    private String userId;
+    private String password;
+    private String email;
+    private String userType;
+    public User(String userId, String password, String email, String userType) {
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.userType = userType;
+    }
+    public void resetPass(){
+        this.password = "";
+    }
+    public User createUser(){
+        return new User(userId, password, email, userType);
+    }
 }
