a9b715b1b017f5486921d5aa2596674b9df3dd08
thien-0408 <nguyendinhthien933@gmail.com>
2025-04-26 09:23:28 +0700

    update getter setter User

--- Lab2/src/models/User.java
+++ Lab2/src/models/User.java
@@ -5,6 +5,39 @@ public class User {
     private String password;
     private String email;
     private String userType;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
     public User(String userId, String password, String email, String userType) {
         this.userId = userId;
         this.password = password;
